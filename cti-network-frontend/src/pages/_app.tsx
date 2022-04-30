99662a5e8da55bcc3550b396d7c24b2ae4f1de1e
nicolas <nasr528@gmail.com>
2022-04-30 18:52:38 +0300

    build error

--- cti-network-frontend/src/pages/_app.tsx
+++ cti-network-frontend/src/pages/_app.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 
 import type { AppProps } from "next/app";
 
+import 'tailwindcss/tailwind.css'
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
