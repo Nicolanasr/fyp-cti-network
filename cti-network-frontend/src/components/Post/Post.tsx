1292b85b943a2e91021bb752bc02a59d79a88746
nicolas <nasr528@gmail.com>
2022-05-01 21:46:10 +0300

    homepage post

--- cti-network-frontend/src/components/Post/Post.tsx
+++ cti-network-frontend/src/components/Post/Post.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from "next/image"
 import Link from "next/link"
 
@@ -25,6 +25,13 @@ export interface PostType {
 }
 
 const Post = ({ author_name, author_pic, id, post_url, posted_date, text, comments, images, likes_count }: PostType) => {
+    const [readMoreText, setReadMoreText] = useState<boolean>(false)
+    const [userLiked, setUserLiked] = useState<boolean>(false)
+
+    const postLikeHandler = (toSet: boolean) => {
+        setUserLiked(toSet);
+    }
+
     return (
         <div className="flex w-full max-w-3xl">
             <div className="w-10 h-10 relative rounded-full bg-slate-50 overflow-hidden">
@@ -35,57 +42,72 @@ const Post = ({ author_name, author_pic, id, post_url, posted_date, text, commen
                 </Link>
             </div>
             <div className="flex-1 mx-4 ">
-                <Link href={post_url} >
-                    <a >
-                        {/* post header part */}
-                        <div className="h-10">
-                            <div className="mb-1">
-                                <Link href={`/profile/${author_name}`}>
-                                    <a className="font-semibold">{author_name} </a>
-                                </Link>
-                                <span> has posted an update.</span>
-                            </div>
-                            <div className="text-gray-500 text-xs">
-                                {timeSince(posted_date)}
-                            </div>
-                        </div>
-                        {/* post content */}
-                        <div className="w-full ">
-                            <p className="my-4 whitespace-pre-wrap">{text}</p>
+                {/* <Link href={post_url} >
+                    <a > */}
+                {/* post header part */}
+                <div className="h-10">
+                    <div className="mb-1">
+                        <Link href={`/profile/${author_name}`}>
+                            <a className="font-semibold">{author_name} </a>
+                        </Link>
+                        <span> has posted an update.</span>
+                    </div>
+                    <div className="text-gray-500 text-xs">
+                        {timeSince(posted_date)}
+                    </div>
+                </div>
+                {/* post content */}
+                <div className="w-full ">
+                    <p className="my-4 whitespace-pre-wrap">
+                        {
+                            text.length > 170 && !readMoreText ?
+                                <>
+                                    {text.substring(0, 170) + " "}
+                                    <button className="text-secondary-100 z-10"
+                                        onClick={() => setReadMoreText(true)}>
+                                        read more...
+                                    </button>
+                                </>
+                                : text
+                        }
+                    </p>
+                    {
+                        images && images.length > 0 &&
+                        <div className={`aspect-square max-w-xl my-2 grid gap-1 ${images.length === 1 ? "grid-cols-1" : images.length >= 2 ? "grid-cols-2" : ""}`}>
                             {
-                                images && images.length > 0 &&
-                                <div className={`aspect-square max-w-xl my-2 grid gap-1 ${images.length === 1 ? "grid-cols-1" : images.length >= 2 ? "grid-cols-2" : ""}`}>
-                                    {
-                                        images.map((image, img_index) => (
-                                            img_index < 4 &&
-                                            <div key={image + img_index} className="h-full w-full relative">
-                                                <Image src={image} layout="responsive" height={100} width={100} objectFit="cover" objectPosition="center" alt={"post image " + image} />
-                                            </div>
-                                        ))
-                                    }
-                                </div>
+                                images.map((image, img_index) => (
+                                    img_index < 4 &&
+                                    <div key={image + img_index} className="h-full w-full relative">
+                                        <Image src={image} layout="responsive" height={100} width={100} objectFit="cover" objectPosition="center" alt={"post image " + image} />
+                                    </div>
+                                ))
                             }
                         </div>
-                    </a >
-                </Link >
+                    }
+                </div>
+                {/*</a >
+                </Link > */}
+
                 {/* post actions bar */}
                 <div className="my-3 py-3 border-y flex gap-4 text-sm">
                     {/* like */}
-                    <button className="flex items-center">
+                    <button className="flex items-center" onClick={() => postLikeHandler(!userLiked)}>
                         {/* icon */}
-                        <span className="relative" style={{ top: "1px" }}>
-                            <Image src="/svg/hand-thumbs-up.svg" layout='fixed' height={16} width={16} objectFit="contain" alt="like" />
+                        <span className={`relative ${userLiked ? "text-secondary-100" : ""}`} style={{ top: "-2px" }}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2.144 2.144 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a9.84 9.84 0 0 0-.443.05 9.365 9.365 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111L8.864.046zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a8.908 8.908 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.224 2.224 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.866.866 0 0 1-.121.416c-.165.288-.503.56-1.066.56z" />
+                            </svg>
                         </span>
-                        <span className="mx-2 ">Like</span>
+                        <span className={`mx-2 ${userLiked ? "text-secondary-100" : ""}`}>Like</span>
                         {/* count */}
-                        <span className="px-1 text-xs text-gray-500 bg-gray-50 border rounded-full">{likes_count}</span>
+                        <span className={`px-2 text-xs ${userLiked ? "text-secondary-100 bg-secondary-100/[0.1]" : "text-gray-500 bg-gray-100"} rounded-full`}>{likes_count}</span>
                     </button>
                     {/* comment */}
                     <button className="flex items-center">
                         <span className="mx-2 ">Comment</span>
                         {
                             comments &&
-                            <span className="px-1 text-xs text-gray-500 bg-gray-50 border rounded-full">{comments.length}</span>
+                            <span className={`px-2 text-xs text-gray-500 bg-gray-100 rounded-full`}>{comments.length}</span>
                         }
                     </button>
                     {/* share */}
@@ -93,8 +115,17 @@ const Post = ({ author_name, author_pic, id, post_url, posted_date, text, commen
                         <span className="mx-2 ">Share</span>
                     </button>
                 </div>
+
+                {/* post comment */}
+                <div className="w-full">
+                    {
+                        comments && comments?.length > 0 ? <></>
+                            :
+                            <>No comments</>
+                    }
+                </div>
             </div>
-        </div>
+        </div >
     )
 }
 
