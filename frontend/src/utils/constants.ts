4ea6b43c0ead226804ad878a0fbe2d145d0fbb15
nicolas <nasr528@gmail.com>
2022-05-03 15:56:53 +0300

    heroku backend connection

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,2 +1,6 @@
 export const API_URL =
-	process.env.NODE_ENV === "development" ? "http://localhost:8000" : process.env.NODE_ENV === "production" ? "http://localhost:8000" : null;
+	process.env.NODE_ENV === "development"
+		? "http://localhost:8000"
+		: process.env.NODE_ENV === "production"
+		? "https://cti-network.herokuapp.com"
+		: null;
