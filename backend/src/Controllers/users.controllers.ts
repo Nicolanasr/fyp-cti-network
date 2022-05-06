9264eca434ce4e79f607305be415c1f64fff4915
nicolasnasr <nasr528@gmail.com>
2022-05-06 12:15:56 +0300

    try cookei

--- backend/src/Controllers/users.controllers.ts
+++ backend/src/Controllers/users.controllers.ts
@@ -60,13 +60,13 @@ export const signin = async (req: Request, res: Response): Promise<void> => {
 										res.status(500).json({ success: false, message: `jwt error -- ${jwtErr}` });
 										return;
 									}
-									// res.cookie("token", jwtToken, {
-									// 	expires: new Date(Date.now() + (process.env.tokenExp ? parseInt(process.env.tokenExp) : 604800) * 1000), // time until expiration in ms
-									// 	secure: true,
-									// 	httpOnly: true,
-									// 	sameSite: "none",
-									// 	path: "/",
-									// });
+									res.cookie("token", jwtToken, {
+										expires: new Date(Date.now() + (process.env.tokenExp ? parseInt(process.env.tokenExp) : 604800) * 1000), // time until expiration in ms
+										secure: true,
+										httpOnly: true,
+										sameSite: "none",
+										path: "/",
+									});
 									res.status(200).json({
 										success: true,
 										data: existUser,
