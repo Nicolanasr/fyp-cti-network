80be7e63cc87cd3b10b9bc0abed0cf9ef95b3def
nicolas <nasr528@gmail.com>
2022-05-07 16:21:25 +0300

    ..

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -2,5 +2,5 @@ export const API_URL =
     process.env.NODE_ENV === "development"
         ? "http://localhost:8000"
         : process.env.NODE_ENV === "production"
-        ? "https://fyp-cti-network-backend.vercel.app"
+        ? "https://cti-network.herokuapp.com"
         : null;
