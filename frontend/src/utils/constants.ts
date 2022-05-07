2f8df2e32b6383668e0a97471c4a73250876d30d
nicolas <nasr528@gmail.com>
2022-05-07 16:15:29 +0300

    change api url to vercel

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,6 +1,6 @@
 export const API_URL =
-	process.env.NODE_ENV === "development"
-		? "http://localhost:8000"
-		: process.env.NODE_ENV === "production"
-		? "https://cti-network.herokuapp.com"
-		: null;
+    process.env.NODE_ENV === "development"
+        ? "http://localhost:8000"
+        : process.env.NODE_ENV === "production"
+        ? "https://fyp-cti-network-backend.vercel.app"
+        : null;
